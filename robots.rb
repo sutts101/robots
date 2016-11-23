3d9fd51c771ae102b6801c747f55ef20ce6db711
Ian Sutton <sutts101@gmail.com>
2016-11-23 08:43:46 +0800

    Dumb Point class to hold x and y coords (yeah, probably pullable-inable from somewhere...)

---
+++ robots.rb
@@ -0,0 +1,10 @@
+class Point
+
+  attr_reader :x, :y
+
+  def initialize(x, y)
+    @x = Integer x
+    @y = Integer y
+  end
+
+end
--- robots_spec.rb
+++ robots_spec.rb
@@ -1,7 +1,21 @@
-describe 'monkey' do
+require './robots.rb'
 
-  it 'should fail' do
-    expect(true).to eq false
+describe Point do
+
+  describe '#new' do
+    it 'should be happy if you give it integer values for x and y' do
+      p = Point.new 2,3
+      expect(p.x).to eq 2
+      expect(p.y).to eq 3
+    end
+    it 'should also be happy if you give it string integer values for x and y' do
+      p = Point.new '2','3'
+      expect(p.x).to eq 2
+      expect(p.y).to eq 3
+    end
+    it 'should complain if coords are not ints' do
+      expect{Point.new 'apple', 'banana'}.to raise_error(ArgumentError)
+    end
   end
 
 end
