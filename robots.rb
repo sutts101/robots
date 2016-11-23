b0365ac492ecb6993105ab048efe50739fb6326e
Ian Sutton <sutts101@gmail.com>
2016-11-23 08:54:34 +0800

    Orientation and move

--- robots.rb
+++ robots.rb
@@ -7,4 +7,37 @@ class Point
     @y = Integer y
   end
 
+  def to_a
+    [@x,@y]
+  end
+
 end
+
+class Orientation
+
+  def self.north
+    @north ||= Orientation.new 0, 1
+  end
+
+  def self.east
+    @east ||= Orientation.new 1, 0
+  end
+
+  def self.south
+    @south ||= Orientation.new 0, -1
+  end
+
+  def self.west
+    @west ||= Orientation.new -1, 0
+  end
+
+  def initialize(x_delta, y_delta)
+    @x_delta, @y_delta = x_delta, y_delta
+  end
+
+  def move(point)
+    Point.new point.x + @x_delta, point.y + @y_delta
+  end
+
+end
+
--- robots_spec.rb
+++ robots_spec.rb
@@ -18,4 +18,24 @@ describe Point do
     end
   end
 
+  describe '#to_a' do
+    it 'should output x and y in an array' do
+      expect(Point.new(1,2).to_a).to eq [1,2]
+    end
+  end
+
+end
+
+describe Orientation do
+
+  describe '#move' do
+    it 'should return a new point transformed (by 1) in the appropriate direction' do
+      p = Point.new 0,0
+      expect(Orientation.north.move(p).to_a).to eq [0,1]
+      expect(Orientation.south.move(p).to_a).to eq [0,-1]
+      expect(Orientation.east.move(p).to_a).to eq [1,0]
+      expect(Orientation.west.move(p).to_a).to eq [-1,0]
+    end
+  end
+
 end
